22b5fbe559e3b79df42e470d0e904f925b98ca46
nson <you@example.com>
2019-05-26 21:33:17 +0700

    Finish fixing jquery + Chang jquery code to JS code

--- home/home.js
+++ home/home.js
@@ -1,17 +1,26 @@
 //This simple script handles the header nav menu modal
 //also covered nav sub menu selection
 //Dependencies: jQuery
-$(document).ready(function() {
 
-	$('.burger').click(function(){
-		$('header').toggleClass('clicked');
+console.log('home js');
+document.addEventListener('DOMContentLoaded', () => {
+	let burger = document.getElementsByClassName('burger');
+	Array.prototype.forEach.call(burger, function(b) {
+		b.addEventListener('click', () => {
+			let header = document.querySelector('header');
+			header.classList.toggle("clicked");
+		})
 	});
-
-	$('nav ul li').click(function(){
-		$('nav ul li').removeClass('selected');
-		$('nav ul li').addClass('notselected');
-		$(this).toggleClass('selected');
-		$(this).removeClass('notselected');
+	let li = document.querySelectorAll('nav ul li');
+	Array.prototype.forEach.call(li, function(l) {
+		l.addEventListener('click', (event) => {
+			let li1 = document.querySelectorAll('nav ul li');
+			Array.prototype.forEach.call(li1, function(l1) {
+				l1.classList.remove('selected');
+				l1.classList.add('notselected');
+			});
+			event.path[1].classList.toggle('selected');
+			event.path[1].classList.remove('notselected');
+		})
 	});
-	
-});
+})
