303205405761e8ba400c039ffe3d97e5f0c3175a
nson <you@example.com>
2019-05-27 20:31:24 +0700

    update password

--- login/js/password.js
+++ login/js/password.js
@@ -1,12 +1,27 @@
-$(document).ready(function(){
-    $('.pass_show').append('<span class="ptxt">Show</span>');  
-    });
+console.log('password js');
+
+// $(document).ready(function(){
+//     $('.pass_show').append('<span class="ptxt">Show</span>');  
+//     });
       
     
-    $(document).on('click','.pass_show .ptxt', function(){ 
+//     $(document).on('click','.pass_show .ptxt', function(){ 
     
-    $(this).text($(this).text() == "Show" ? "Hide" : "Show"); 
+//     $(this).text($(this).text() == "Show" ? "Hide" : "Show"); 
     
-    $(this).prev().attr('type', function(index, attr){return attr == 'password' ? 'text' : 'password'; }); 
+//     $(this).prev().attr('type', function(index, attr){return attr == 'password' ? 'text' : 'password'; }); 
     
-    });
+//     });
+
+document.addEventListener('DOMContentLoaded', () => {
+    let password_show = document.getElementsByClassName('ptxt');
+    console.log(password_show);
+    password_show.addEventListener('click', () => {
+        let password = document.getElementById('myInput');
+        if (password.type === 'password') {
+            password.type = 'text';
+        } else {
+            password.type = 'password';
+        }
+    })
+})
