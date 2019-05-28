8dfb9927f43aa2e3d411e875be1734959dd1cba7
nson <you@example.com>
2019-05-28 20:13:39 +0700

    Add feature: Disable 'Submit' until form filled + Register page + Login page

--- login/register/js/register_password.js
+++ login/register/js/register_password.js
@@ -1,13 +1,29 @@
-let eye = document.getElementById('password_eye');
-eye.addEventListener('click', () => {
-    var pass_input = document.getElementById("password");
+console.log('register_password JS');
+
+let eye1 = document.getElementById('password_eye1');
+eye1.addEventListener('click', () => {
+    var pass_input = document.getElementById("password1");
     if (pass_input.type === "password") {
         pass_input.type = "text";
-        eye.classList.add('fa-eye-slash');
-        eye.classList.remove('fa-eye');
+        eye1.classList.add('fa-eye-slash');
+        eye1.classList.remove('fa-eye');
         } else {
         pass_input.type = "password";
-        eye.classList.remove('fa-eye-slash');
-        eye.classList.add('fa-eye');
+        eye1.classList.remove('fa-eye-slash');
+        eye1.classList.add('fa-eye');
+        }
+})
+
+let eye2 = document.getElementById('password_eye2');
+eye2.addEventListener('click', () => {
+    var pass_input = document.getElementById("password2");
+    if (pass_input.type === "password") {
+        pass_input.type = "text";
+        eye2.classList.add('fa-eye-slash');
+        eye2.classList.remove('fa-eye');
+        } else {
+        pass_input.type = "password";
+        eye2.classList.remove('fa-eye-slash');
+        eye2.classList.add('fa-eye');
         }
 })
