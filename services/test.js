4949d9df87fe0d711a3dcf594a68fc5265498d6f
nson <you@example.com>
2019-06-08 22:12:11 +0700

    doing api call

--- services/test.js
+++ services/test.js
@@ -1,10 +1,33 @@
 // var loginForm = document.getElementById('addPost');
 var getAPIBtn = document.getElementById('getUser');
+var form = document.getElementById('addPost');
 
-getAPIBtn.addEventListener('click', async () => {
+getAPIBtn.addEventListener('click', () => {
     console.log('clicked');
 
     fetch(`https://polar-hollows-67198.herokuapp.com/api/v1/weapons`)
-    .then(res => res.json())          // convert to plain text
-    .then(data => console.log(data[]));
+    .then(res => res.json())
+    .then(data => console.log(data[1].icon));
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    // let username = 'nhatlq';
+    // let password = '!@#!@#$@#$';
+ 
+    var url = 'https://polar-hollows-67198.herokuapp.com/api/v1/users?username=nhatlq&password=sasdas' + 
+    
+    fetch(url, {
+        method:'POST',
+        mode:'no-cors',
+        headers: {
+            'Content-type':'application/json',
+        },
+    })
+    .then(res => res.text())
+    .then(data => console.log(data))    
 });
